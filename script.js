33723dc7e873445274f081af3a57089a355e04e5
bishopcccc <bishopc@limestone.on.ca>
2026-02-06 09:30:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const image = document.getElementByID("richLaptop.png")
+const image = document.getElementByID("laptopImage");
+const text = document.getElementById("laptopText");
+const button = document.getElementById("spinButton");
+
+const imageOptions = [
+  ["richLaptop.png", "Cole's Laptop"],
+  ["poorLaptop.png", "Matthew's Laptop"],
+];
+
+let imageSource = "";
+let text = "";
+let isSpinning = false;
+
+function spinWheel() {
+  if (isSpinning == false) {
+    isSpinning = true;
+
+    for (i = 0; i < 10; i++) {
+      image.src = 
+    }
+  }
+};
+
+button.addEventListener("click", spinWheel);
